dbb49fb1c1ebbdc51124704f63a77c3f58427629
SANJIKS <sanzarmaratov588@gmail.com>
2024-09-10 17:46:01 +0600

    push

--- core/settings.py
+++ core/settings.py
@@ -15,6 +15,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS','').split(',')
 
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -31,6 +32,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
